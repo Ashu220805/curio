8d9de7c08a7b867ea64dc52cca72e786d010ffc4
Ashu_220805 <ashishsingh220805@gmail.com>
2026-09-02 14:34:16 +0530

    fix: increase Vite chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -6,23 +6,5 @@ export default defineConfig({
 
   build: {
     chunkSizeWarningLimit: 1000,
-
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "react-vendor";
-            }
-
-            if (id.includes("@supabase")) {
-              return "supabase";
-            }
-
-            return "vendor";
-          }
-        },
-      },
-    },
   },
 });
